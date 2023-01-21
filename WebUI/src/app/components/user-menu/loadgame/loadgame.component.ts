f546413f93b3cfff6cd69234ee2c520139b52d95
SunS33t <deniska355466@gmail.com>
2023-01-21 15:34:25 +0400

    fix game, add winner function

--- WebUI/src/app/components/user-menu/loadgame/loadgame.component.ts
+++ WebUI/src/app/components/user-menu/loadgame/loadgame.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+declare function startGame():any;
+declare function setParameters(w:Number,h:Number,r:Number,c:Number,g:Number,p:String):any;
 
 @Component({
   selector: 'app-loadgame',
@@ -10,6 +11,11 @@ export class LoadgameComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    startGame();
+    setParameters(30,30,3,3,0,"url(https://cq.ru/storage/uploads/posts/1020636/4bada763f83c45653c9ffb4c8fa22091.jpg)");
+  }
   
+  ngOnChange(){
+    console.log();
   }
 }
